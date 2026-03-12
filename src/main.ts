2a3c63710d7c7ae3d8a99966966909b45002a326
RonaldoCMS <ronaldocms@outlook.it>
2026-03-12 23:32:38 +0100

    feat: add logging, transform and timeout interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+   app.useGlobalInterceptors(
+    new LoggingInterceptor(),
+    new TransformInterceptor(),
+    new TimeoutInterceptor(),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
